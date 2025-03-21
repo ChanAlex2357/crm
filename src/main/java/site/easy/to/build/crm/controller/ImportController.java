d3164a1674bad5b168d992585c19484233677b64
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-21 11:09:11 +0300

    init the import controller

--- src/main/java/site/easy/to/build/crm/controller/ImportController.java
+++ src/main/java/site/easy/to/build/crm/controller/ImportController.java
@@ -1,19 +1,13 @@
 package site.easy.to.build.crm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class ImportController {
-    @Autowired
-    private ImportService importService;
-
-/*************  ✨ Codeium Command ⭐  *************/
-    /**
-     * This function imports data from a CSV file to the database.
-     * @param file a CSV file containing data
-     * @return a redirect to the home page
-     */
-/******  87b826b3-d179-46fd-9cea-803613dc7ab5  *******/    @RequestMapping("/import")
-    public String importData(@RequestParam("file") MultipartFile file) {
-        importService.importData(file);
-        return "redirect:/";
-    }    
+    
+    @GetMapping("/import")
+    public String importForm(){
+        return "import/import-lead";
+    }
 }

a3745c12b3513a6b51efed2f791d34b1e018a54d
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-21 14:59:45 +0300

    service for parsing csv into Mapping Class

---
+++ src/main/java/site/easy/to/build/crm/service/imp/CSVService.java
@@ -0,0 +1,34 @@
+package site.easy.to.build.crm.service.imp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CSVService<T> {
+    public List<T> parseCSV(MultipartFile file,Class<T> clazz,char separator){
+        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader)
+                .withType(clazz)
+                .withIgnoreLeadingWhiteSpace(true)
+                .withIgnoreEmptyLine(true)
+                .withSeparator(separator)
+                .build();
+            
+            List<T> csvRecords = csvToBean.parse();
+            return csvRecords;
+        } catch (IOException e) {
+            log.error("Error parsing CSV file", e);
+            throw new RuntimeException("Failed to process CSV file: " + e.getMessage());
+        }
+    }
+}

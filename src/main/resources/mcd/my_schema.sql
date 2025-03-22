b41dcfc1d31e76f92061aa0501bd54cf2ac1ce61
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-22 20:38:34 +0300

    custom table expense alert

--- src/main/resources/mcd/my_schema.sql
+++ src/main/resources/mcd/my_schema.sql
@@ -42,3 +42,41 @@ ALTER TABLE customer_expense ADD COLUMN `lead` TINYINT(1);
 alter TABLE customer_expense ADD COLUMN `ticket` TINYINT(1);
 ALTER TABLE customer_expense MODIFY COLUMN lead_id INT unsigned NULL;
 ALTER TABLE customer_expense MODIFY COLUMN ticket_id INT unsigned NULL;
+
+
+CREATE TABLE alert_type(
+   id INT AUTO_INCREMENT,
+   val VARCHAR(100) ,
+   desce VARCHAR(100) ,
+   PRIMARY KEY(id)
+);
+
+
+CREATE TABLE expense_alert(
+   alert_id INT AUTO_INCREMENT,
+   budget_amount DECIMAL(15,2)  ,
+   taux DECIMAL(15,2)  ,
+   message TEXT,
+   expense DECIMAL(15,2)  ,
+   date_alert DATE NOT NULL,
+   status INT,
+   budget_id INT NOT NULL,
+   expense_id INT NOT NULL,
+   alert_type_id INT NOT NULL,
+   PRIMARY KEY(alert_id),
+   FOREIGN KEY(budget_id) REFERENCES customer_budget(budget_id),
+   FOREIGN KEY(expense_id) REFERENCES customer_expense(expense_id),
+   FOREIGN KEY(alert_type_id) REFERENCES alert_type(id)
+);
+
+
+ALTER TABLE expense_alert ADD COLUMN expense_amount DECIMAL(15,2);
+ALTER TABLE expense_alert DROP COLUMN expense ;
+
+
+CREATE TABLE expense_settings(
+   id INT AUTO_INCREMENT,
+   taux DECIMAL(15,2)  ,
+   date_taux VARCHAR(100) ,
+   PRIMARY KEY(id)
+);

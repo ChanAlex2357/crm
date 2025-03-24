4bbf541c76f9a16d4385e443d2d870c9bd0d4302
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-24 16:24:36 +0300

    model ticket for json response

--- src/main/java/site/easy/to/build/crm/entity/dto/TicketDTO.java
+++ src/main/java/site/easy/to/build/crm/entity/dto/TicketDTO.java
@@ -3,10 +3,11 @@ package site.easy.to.build.crm.entity.dto;
 import java.time.LocalDateTime;
 
 public interface TicketDTO {
+    // Ticket basic information
     Integer getTicketId();
     String getSubject();
+    String getStatus();
     String getDescription();
-    String getTicketStatus();
     String getPriority();
     LocalDateTime getCreatedAt();
     

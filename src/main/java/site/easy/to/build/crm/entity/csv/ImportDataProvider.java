051b62802ae734464bbfde79cbc95451687387d4
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-03-25 13:09:37 +0300

    Provide file import service

---
+++ src/main/java/site/easy/to/build/crm/entity/csv/ImportDataProvider.java
@@ -0,0 +1,19 @@
+package site.easy.to.build.crm.entity.csv;
+
+import org.springframework.web.multipart.MultipartFile;
+import lombok.Data;
+import site.easy.to.build.crm.service.csv.ImportCsvService;
+@Data
+public class ImportDataProvider {
+    private MultipartFile file;
+    private ImportCsvService importCsvService;
+    private char separator;
+    public ImportDataProvider(MultipartFile file,ImportCsvService service,char separator){
+        setFile(file);
+        setImportCsvService(service);
+        setSeparator(separator);
+    }
+    public ImportDataProvider(MultipartFile file,ImportCsvService service){
+        this(file, service,';');
+    }
+}
